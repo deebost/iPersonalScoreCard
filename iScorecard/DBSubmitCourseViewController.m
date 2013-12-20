6d5daed8c10eb14fc6a4a77f41f38f40b317bee9
deebost <floggingdan721@yahoo.com>
2013-12-20 11:29:43 -0600

    took out giant fucking method! lol
    :D made me feell good! that method was embarrassing!

--- iScorecard/DBSubmitCourseViewController.m
+++ iScorecard/DBSubmitCourseViewController.m
@@ -33,7 +33,36 @@
 @property (weak, nonatomic) IBOutlet UITextField *hole18TextField;
 @property (weak, nonatomic) IBOutlet UIButton *submitCourseButton;
 @property (weak, nonatomic) IBOutlet UITextField *cityTextfield;
+@property (weak, nonatomic) IBOutlet UIView *topHoleView;
 @property (weak, nonatomic) IBOutlet UITextField *enterStateTextField;
+@property (weak, nonatomic) IBOutlet UIView *allHoleView;
+- (IBAction)courseNameAction:(id)sender;
+- (IBAction)courseStateAction:(id)sender;
+- (IBAction)courseCityAction:(id)sender;
+
+- (IBAction)hole1EditAction:(id)sender;
+- (IBAction)hole2editAction:(id)sender;
+- (IBAction)hole3editAction:(id)sender;
+- (IBAction)hole4editAction:(id)sender;
+- (IBAction)hole5editAction:(id)sender;
+- (IBAction)hole6editAction:(id)sender;
+- (IBAction)hole7editAction:(id)sender;
+- (IBAction)hole8editAction:(id)sender;
+- (IBAction)hole9editAction:(id)sender;
+
+- (IBAction)hole10editAction:(id)sender;
+- (IBAction)hole11editAction:(id)sender;
+- (IBAction)hole12editAction:(id)sender;
+- (IBAction)hole13editAction:(id)sender;
+- (IBAction)hole14editAction:(id)sender;
+- (IBAction)hole15editAction:(id)sender;
+- (IBAction)hole16editAction:(id)sender;
+- (IBAction)hole17editAction:(id)sender;
+- (IBAction)hole18editAction:(id)sender;
+
+
+
+
 
 
 
@@ -63,55 +92,29 @@ NSString *holeFifthteen;
 NSString *holeSixteen;
 NSString *holeSeventeen;
 NSString *holeEightteen;
+NSUserDefaults *saveCourseInfo;
+CGRect bottomOrigin;
 BOOL enteredValidState;
 
 @implementation DBSubmitCourseViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+        _allHoleView.backgroundColor = [UIColor whiteColor];
     statesArray = [[NSMutableArray alloc] init];
+    bottomOrigin = _allHoleView.frame;
+
     [statesArray addObjectsFromArray:@[@"Alabama", @"Alaska",  @"Arizona", @"Arkansas", @"California", @"Colorado", @"Connecticut", @"Delaware", @"Flordia", @"Georgia", @"Hawaii", @"Idaho", @"Illinois", @"Indiana", @"Iowa", @"Kansas", @"Kentucky", @"Louisiana", @"Maine", @"Maryland", @"Massachusetts", @"Michigan", @"Minnesota", @"Mississippi", @"Missouri", @"Montana", @"Nebraska", @"Nevada", @"New Hampshire", @"New Jersey", @"New Mexico", @"New York", @"North Carolina", @"North Dakota", @"Ohio", @"Oklahoma", @"Oregon",@"Pennsylvania", @"Rhode Island", @"South Carolina", @"South Dakota", @"Tennessee", @"Texas", @"Utah", @"Vermont", @"Virginia", @"Washington", @"West Virginia", @"Wisconsin", @"Wyoming"]];
 }
 
-- (void) saveToParse {
-    PFUser *currentUser = [PFUser currentUser];
-    PFObject *myScore = [PFObject objectWithClassName:@"Courses"];
 
 
+- (void) addALLPar {
+    NSUInteger result = _hole1TextField.text.integerValue + _hole2TextField.text.integerValue + _hole3TextField.text.integerValue + _hole4TextField.text.integerValue + _hole5TextField.text.integerValue + _hole6TextField.text.integerValue + _hole7TextField.text.integerValue + _hole8TextField.text.integerValue + _hole9TextField.text.integerValue + _hole10TextField.text.integerValue + _hole11TextField.text.integerValue + _hole12TextField.text.integerValue + _hole13TextField.text.integerValue + _hole14TextField.text.integerValue + _hole15TextField.text.integerValue + _hole16TextField.text.integerValue + _hole17TextField.text.integerValue + _hole18TextField.text.integerValue;
+        _totalParTextField.text = [NSString stringWithFormat:@"%i", result];
 
-    NSMutableArray *currentCourseInfo = [[NSMutableArray alloc] init];
-
-
-    [currentCourseInfo addObject:courseName];
-    [currentCourseInfo addObject:courseState];
-    [currentCourseInfo addObject:courseCity];
-        [currentCourseInfo addObject:totalParForCourse];
-    [currentCourseInfo addObject:holeOne];
-    [currentCourseInfo addObject:holeTwo];
-    [currentCourseInfo addObject:holeThree];
-    [currentCourseInfo addObject:holeFour];
-    [currentCourseInfo addObject:holeFive];
-    [currentCourseInfo addObject:holeSix];
-    [currentCourseInfo addObject:holeSeven];
-    [currentCourseInfo addObject:holeEight];
-    [currentCourseInfo addObject:holeNine];
-    [currentCourseInfo addObject:holeTen];
-    [currentCourseInfo addObject:holeEleven];
-    [currentCourseInfo addObject:holeTwelve];
-    [currentCourseInfo addObject:holeThirteen];
-    [currentCourseInfo addObject:holeFourteen];
-    [currentCourseInfo addObject:holeFifthteen];
-    [currentCourseInfo addObject:holeSixteen];
-    [currentCourseInfo addObject:holeSeventeen];
-    [currentCourseInfo addObject:holeEightteen];
-    myScore[@"user"] = currentUser;
-    myScore[@"courseInfo"] = currentCourseInfo;
-    myScore[@"courseState"] = courseState;
-    myScore[@"courseCity"] = courseCity;
-    myScore[@"courseName"] = courseName;
-    myScore[@"coursePar"] = totalParForCourse;
-    [myScore saveInBackground];
 }
+
 
 - (void) getTextOfTextFields {
     courseName = _courseNameTextField.text;
@@ -136,6 +139,7 @@ BOOL enteredValidState;
     holeSixteen = _hole16TextField.text;
     holeSeventeen = _hole17TextField.text;
     holeEightteen = _hole18TextField.text;
+    [self addALLPar];
     NSMutableDictionary *courseInfo = [[NSMutableDictionary alloc] init];
     [courseInfo setObject:holeOne forKey:@"hole1"];
     [courseInfo setObject:holeTwo forKey:@"hole2"];
@@ -160,13 +164,26 @@ BOOL enteredValidState;
     NSArray *tempArray = [[NSArray alloc] initWithObjects:courseInfo, nil];
 
 
-    NSUserDefaults *saveCourseInfo = [NSUserDefaults standardUserDefaults];
+    saveCourseInfo = [NSUserDefaults standardUserDefaults];
     [saveCourseInfo setObject:tempArray forKey:@"allCourses"];
     [saveCourseInfo setObject:courseInfo forKey:[NSString stringWithFormat:@"%@",courseName]];
     [saveCourseInfo setObject:courseName forKey:@"courseName"];
     courseName = _passCourseName;
 
-//    [self saveToParse];
+    [saveCourseInfo synchronize];
+    PFUser *user = [PFUser currentUser];
+    PFObject *myScore = [PFObject objectWithClassName:@"Courses"];
+
+
+
+
+    myScore[@"courseInfo"] = courseInfo;
+    myScore[@"courseState"] = courseState;
+    myScore[@"courseCity"] = courseCity;
+    myScore[@"courseName"] = _courseNameTextField.text;
+    myScore[@"coursePar"] = totalParForCourse;
+    myScore[@"user"] = user;
+    [myScore saveInBackground];
 
 
     UIAlertView *startRoundAlert = [[UIAlertView alloc] initWithTitle:@"Start Round"
@@ -175,10 +192,43 @@ BOOL enteredValidState;
                                                     cancelButtonTitle:nil
                                                     otherButtonTitles:@"Start Round", nil];
     [startRoundAlert show];
-    [saveCourseInfo synchronize];
+
 
 }
 
+
+- (void)moveViewUpForKeyBoard {
+
+
+    [UIView animateWithDuration:1.0 animations:^{
+        _allHoleView.backgroundColor = [UIColor whiteColor];
+
+        _allHoleView.frame = CGRectMake(0, _topHoleView.frame.origin.y, _allHoleView.frame.size.width, _allHoleView.frame.size.width);
+        _topHoleView.alpha = 0.0f;
+    }];
+}
+
+- (void)helpUser:(int)holeValue textField:(UITextField *)holeNumber {
+
+
+    if (holeValue <= 2 ) {
+        holeNumber.text = @"3";
+    } else if (holeValue >= 6) {
+        holeNumber.text = @"5";
+    }
+}
+
+- (void)moveViewDownForKeyBoard {
+            _topHoleView.hidden = NO;
+
+    [UIView animateWithDuration:1.0 animations:^{
+        _allHoleView.backgroundColor = [UIColor whiteColor];
+        _allHoleView.frame = bottomOrigin;
+        _topHoleView.alpha = 1.0f;
+
+    }];
+
+}
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
 
         [self getTextOfTextFields];
@@ -186,36 +236,129 @@ BOOL enteredValidState;
     fhVC.passedCourseName = _passCourseName;
 }
 
-- (void)viewDidDisappear:(BOOL)animated {
-    [super viewDidDisappear:animated];
-
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
 }
 
-- (IBAction)backGroundTouched:(id)sender {
+- (IBAction)courseNameAction:(id)sender {
+        [self addALLPar];
     [_courseNameTextField resignFirstResponder];
-    [_totalParTextField resignFirstResponder];
+}
+
+- (IBAction)courseStateAction:(id)sender {
+        [self addALLPar];
     [_enterStateTextField resignFirstResponder];
+}
+
+- (IBAction)courseCityAction:(id)sender {
+        [self addALLPar];
     [_cityTextfield resignFirstResponder];
+}
+
+- (IBAction)hole1EditAction:(id)sender {
+    [self helpUser:_hole1TextField.text.integerValue textField:_hole1TextField];
+        [self addALLPar];
     [_hole1TextField resignFirstResponder];
+}
+- (IBAction)hole2editAction:(id)sender {
+    [self helpUser:_hole2TextField.text.integerValue textField:_hole2TextField];
+    [self addALLPar];
     [_hole2TextField resignFirstResponder];
+
+}
+- (IBAction)hole3editAction:(id)sender {
+    [self helpUser:_hole3TextField.text.integerValue textField:_hole3TextField];
+    [self addALLPar];
     [_hole3TextField resignFirstResponder];
+
+}
+- (IBAction)hole4editAction:(id)sender {
+    [self helpUser:_hole4TextField.text.integerValue textField:_hole4TextField];
+    [self addALLPar];
     [_hole4TextField resignFirstResponder];
+
+}
+- (IBAction)hole5editAction:(id)sender{
+    [self helpUser:_hole5TextField.text.integerValue textField:_hole5TextField];
+    [self addALLPar];
     [_hole5TextField resignFirstResponder];
-    [_hole6TextField resignFirstResponder];
+}
+- (IBAction)hole6editAction:(id)sender{
+    [self helpUser:_hole6TextField.text.integerValue textField:_hole6TextField];
+    [self addALLPar];
+    [self moveViewUpForKeyBoard];
+       [_hole6TextField resignFirstResponder];
+}
+- (IBAction)hole7editAction:(id)sender{
+    [self helpUser:_hole7TextField.text.integerValue textField:_hole7TextField];
+    [self addALLPar];
+    [self moveViewUpForKeyBoard];
     [_hole7TextField resignFirstResponder];
+}
+- (IBAction)hole8editAction:(id)sender{
+    [self helpUser:_hole8TextField.text.integerValue textField:_hole8TextField];
+    [self addALLPar];
+    [self moveViewUpForKeyBoard];
     [_hole8TextField resignFirstResponder];
+}
+- (IBAction)hole9editAction:(id)sender {
+    [self helpUser:_hole9TextField.text.integerValue textField:_hole9TextField];
+    [self addALLPar];
+    [self moveViewDownForKeyBoard];
     [_hole9TextField resignFirstResponder];
+}
+
+- (IBAction)hole10editAction:(id)sender {
+    [self helpUser:_hole10TextField.text.integerValue textField:_hole10TextField];
+    [self addALLPar];
     [_hole10TextField resignFirstResponder];
+}
+- (IBAction)hole11editAction:(id)sender {
+    [self helpUser:_hole11TextField.text.integerValue textField:_hole11TextField];
+    [self addALLPar];
     [_hole11TextField resignFirstResponder];
+}
+- (IBAction)hole12editAction:(id)sender {
+    [self helpUser:_hole12TextField.text.integerValue textField:_hole12TextField];
+    [self addALLPar];
     [_hole12TextField resignFirstResponder];
+}
+- (IBAction)hole13editAction:(id)sender {
+    [self helpUser:_hole13TextField.text.integerValue textField:_hole13TextField];
+    [self addALLPar];
     [_hole13TextField resignFirstResponder];
+}
+- (IBAction)hole14editAction:(id)sender {
+    [self helpUser:_hole14TextField.text.integerValue textField:_hole14TextField];
+    [self addALLPar];
     [_hole14TextField resignFirstResponder];
+
+}
+- (IBAction)hole15editAction:(id)sender {
+    [self helpUser:_hole15TextField.text.integerValue textField:_hole15TextField];
+    [self addALLPar];
+    [self moveViewUpForKeyBoard];
     [_hole15TextField resignFirstResponder];
+
+}
+- (IBAction)hole16editAction:(id)sender {
+    [self helpUser:_hole16TextField.text.integerValue textField:_hole16TextField];
+    [self addALLPar];
+    [self moveViewUpForKeyBoard];
     [_hole16TextField resignFirstResponder];
+
+}
+- (IBAction)hole17editAction:(id)sender {
+    [self helpUser:_hole17TextField.text.integerValue textField:_hole17TextField];
+    [self addALLPar];
+    [self moveViewUpForKeyBoard];
     [_hole17TextField resignFirstResponder];
+}
+- (IBAction)hole18editAction:(id)sender {
+    [self helpUser:_hole18TextField.text.integerValue textField:_hole18TextField];
+    [self addALLPar];
+    [self moveViewDownForKeyBoard];
     [_hole18TextField resignFirstResponder];
 }
-
-    
 
 @end
